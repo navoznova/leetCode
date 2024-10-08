083c9bb36c3fff67c3b6f9b15e09e04c930477cf
Anna Navoznova <anya.navoznova@gmail.com>
2024-10-08 12:56:36 -0600

    sumOfTwoIndex On

--- Easy/sumOfTwoIndex.ts
+++ Easy/sumOfTwoIndex.ts
@@ -8,4 +8,14 @@ function twoSum(nums, target) {
   }
 }
 
-twoSum([3, 2, 4], 5);
+function twoSumHash(nums, target) {
+  const hashTable = new Map(nums.map((value, index) => [value, index ]));
+  for (var i = 0; i < nums.length; i++) {
+    if (hashTable.has(target - nums[i]) && hashTable.get(target - nums[i]) !== i){
+      return [i, hashTable.get(target - nums[i])];
+    }
+  }
+  return [];
+}
+
+twoSumHash([3, 2, 4], 6);
