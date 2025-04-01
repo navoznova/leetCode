b038c674b08cd23198e063a72b5daddad21479d7
Anna Navoznova <anya.navoznova@gmail.com>
2025-04-01 15:34:58 +0400

    mergeTwoSortedLists worked

--- Easy/mergeTwoSortedArray.ts
+++ Easy/mergeTwoSortedArray.ts
@@ -5,7 +5,26 @@ let m = 3;
 let nums2 = [2, 5, 6];
 let n = 3;
 
-function mergeTwoSortedArray(arr1, arrLength1, arr2, arrLength2) {
+function mergeTwoSortedArray(arr1, m, arr2, n) {
+  let lastArr1Elem = arr1[m-1];//3
+  let lastArr2Elem = arr2[n-1];//2
+  let countCommon = 1;
+  let countArr1 = 1;
+
+  for (let i = n-1; i >= 0; i--) {
+    lastArr2Elem = arr2[i];
+
+    if(lastArr1Elem < lastArr2Elem) {
+      arr1[m+n-countCommon] = lastArr2Elem;
+    } else {
+      arr1[m+n-countCommon] = lastArr1Elem;
+      arr1[m-countArr1] = lastArr2Elem;
+      countArr1++;
+      lastArr1Elem = arr1[m-countArr1];
+    }
+
+    countCommon++;
+  }
  
 }
 
