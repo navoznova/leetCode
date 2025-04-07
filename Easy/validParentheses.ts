b65606c06549ad0c07d34e4c82259528f6c5cb60
Anna Navoznova <anya.navoznova@gmail.com>
2025-04-07 13:06:41 +0400

    validParentheses

--- Easy/validParentheses.ts
+++ Easy/validParentheses.ts
@@ -7,23 +7,61 @@
 // Every close bracket has a corresponding open bracket of the same type.
 
 function validParentheses(str) {
-    const stack: string[] = [];
-    const brackets = {
-        ')': '(',
-        '}': '{',
-        ']': '['};
+  const vocabulary = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
 
-    for (let char of str) {
-        if(brackets[char]) {
-            const lastElement = stack.pop();
-            if (brackets[char] !== lastElement) { // Проверяем соответствие открывающей скобки
-                return false;
-            }
-        } else {
-            stack.push(char);
-        }
+  const res = [];
+
+  for (let char of str) {
+    let lastElemInRes = res[res.length-1];
+    if (vocabulary[char]) {
+      res.push(char);
+    } else if (res.length === 0 && !vocabulary[char]) {
+      return false;
+    } else if (vocabulary[lastElemInRes] === char) {
+      res.pop();
     }
-    return stack.length === 0;
-};
+  }
 
-validParentheses('{}')
+  return !res.length;
+}
+
+// function validParentheses(str) {
+//   const templeOpen = "(";
+//   const res = [];
+
+//   for (let char of str) {
+//     if (res.length === 0 && char !== templeOpen) {
+//       return false;
+//     }
+
+//     char === templeOpen ? res.push(char) : res.pop();
+//   }
+
+//   return !res.length;
+// }
+
+// function validParentheses(str) {
+//     const stack = [];
+//     const brackets = {
+//         ')': '(',
+//         '}': '{',
+//         ']': '['};
+
+//     for (let char of str) {
+//         if(brackets[char]) {
+//             const lastElement = stack.pop();
+//             if (brackets[char] !== lastElement) { // Проверяем соответствие открывающей скобки
+//                 return false;
+//             }
+//         } else {
+//             stack.push(char);
+//         }
+//     }
+//     return stack.length === 0;
+// };
+
+console.log(validParentheses("({[]})"));
