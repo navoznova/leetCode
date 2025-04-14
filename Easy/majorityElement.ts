65f55568a496544f13c7c2f796905f12d860e933
Anna Navoznova <anya.navoznova@gmail.com>
2025-04-14 13:16:47 +0400

    majorityElement2 solution

--- Easy/majorityElement.ts
+++ Easy/majorityElement.ts
@@ -2,18 +2,28 @@
 //You must implement a solution with a linear runtime complexity and use only constant extra space.
 
 function majorityElement(nums) {
-  let res = {};
-  for (let num of nums) {
-    res[num] ? (res[num] += 1) : (res[num] = 1);
+  let majorElem = 0;
+  let repeats = 0;
+
+  for(let num of nums) {
+    if (repeats === 0) {
+      majorElem = num;
+    }
+
+    repeats += (num === majorElem) ? 1 : -1;
   }
+  // let res = {};
+  // for (let num of nums) {
+  //   res[num] ? (res[num] += 1) : (res[num] = 1);
+  // }
 
-  let repeats = Object.values(res)
-  let maxVal = Math.max(...repeats);
-  let indexOfMaxVal = repeats.indexOf(maxVal);
+  // let repeats = Object.values(res)
+  // let maxVal = Math.max(...repeats);
+  // let indexOfMaxVal = repeats.indexOf(maxVal);
 
-  return Object.keys(res)[indexOfMaxVal];
+  return majorElem;
 }
 
-let nums = [4, 2, 2, 2, 4, 3];
+let nums = [4, 2, 2, 2, 3, 3];
 
 console.log(majorityElement(nums));
