4e879ae9d490d6eb3c3deb4a1237ab3c906b5072
Anna Navoznova <anya.navoznova@gmail.com>
2025-04-14 11:46:56 +0400

    majorityElement1 solution

---
+++ Easy/majorityElement.ts
@@ -0,0 +1,19 @@
+//Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+//You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+function majorityElement(nums) {
+  let res = {};
+  for (let num of nums) {
+    res[num] ? (res[num] += 1) : (res[num] = 1);
+  }
+
+  let repeats = Object.values(res)
+  let maxVal = Math.max(...repeats);
+  let indexOfMaxVal = repeats.indexOf(maxVal);
+
+  return Object.keys(res)[indexOfMaxVal];
+}
+
+let nums = [4, 2, 2, 2, 4, 3];
+
+console.log(majorityElement(nums));
