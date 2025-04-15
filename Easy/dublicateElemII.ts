4489c2e2715e588010af15b9ed221bed52ca009e
Anna Navoznova <anya.navoznova@gmail.com>
2025-04-15 15:16:34 +0400

    DuplicateII on solution

--- Easy/dublicateElemII.ts
+++ Easy/dublicateElemII.ts
@@ -1,10 +1,13 @@
 //Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
 function containsDuplicateII(nums, k) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length && j - i <= k; j++) {
-      if (nums[i] == nums[j]) {
+  let dictionary = new Map();
+
+  for (let i = 0; i<nums.length; i++) {
+    let a = nums[i]
+    if(dictionary.has(a) && i - dictionary.get(nums[i]) <= k) {
         return true;
-      }
+    } else {
+      dictionary.set(nums[i], i);
     }
   }
 
